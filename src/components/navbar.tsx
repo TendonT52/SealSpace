5d6a496d2b7885b85be8cfb45fa9646cde65372f
TendonT52 <tendon.t52@gmail.com>
2023-10-25 15:15:24 +0700

    fix navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -7,14 +7,14 @@ export default function Navbar({ role }: { role: "host" | "renter" | "guest" }) 
       <div className="container flex items-center justify-between">
         <div className="flex items-center gap-3">
           <Banner />
-          <Button text="Explore spaces" type="link" />
-          {(role === "host" || role === "renter") && <Button text="My reservation" type="link" />}
-          {role === "host" && <Button text="My spaces" type="link" />}
+          <Button text="Explore spaces" variant="link" />
+          {(role === "host" || role === "renter") && <Button text="My reservation" variant="link" />}
+          {role === "host" && <Button text="My spaces" variant="link" />}
         </div>
         <div className="flex items-center gap-3">
-          {role === "guest" && <Button text="Register" type="secondary" />}
-          {role === "guest" && <Button text="Login" type="primary" />}
-          {(role === "host" || role === "renter") && <Button text="Logout" type="secondary" />}
+          {role === "guest" && <Button text="Register" variant="secondary" />}
+          {role === "guest" && <Button text="Login" variant="primary" />}
+          {(role === "host" || role === "renter") && <Button text="Logout" variant="secondary" />}
         </div>
       </div>
     </nav>
