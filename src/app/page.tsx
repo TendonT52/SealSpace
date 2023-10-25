0742da3cd381f13203c3128f8677e0fe80c27915
TikhampornSky <tikhamporntan@gmail.com>
2023-10-25 19:06:18 +0700

    complete home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Benefit from "@/components/benefit"
 import Brand from "@/components/brand"
 import Image from "next/image"
 import Link from "next/link"
@@ -22,6 +23,32 @@ export default function Home() {
         </div>
       </div>
 
+      <div className="my-28 flex flex-row items-center justify-center gap-8">
+        <Image src="/home/reservation_list.png" width={570} height={209} alt="home-reservation-list" />
+        <div className="flex flex-col items-start">
+          <Brand text="Streamline" className="text-5xl/[72px]"/>
+          <Brand text="Workspace Management" className="text-5xl/[72px]"/>
+          <Brand text="With Powerful Tools" className="text-5xl/[72px]"/>
+        </div>
+      </div>
+
+      <div className="my-12 flex flex-row items-center justify-center gap-14">
+        <Image src="/home/reservation.png" width={480} height={284} alt="home-reservation-list" />
+        <div className="flex w-[616px] flex-col items-start">
+          <Brand text="Discover and Reserve the Perfect Co-Working Space for Your Needs" className="text-left"/>
+          <br></br>
+          <p className="text-navy"> 
+            Our platform makes it easy to find and book co-working spaces that meet
+            your requirements. With just a few clicks, you can secure a workspace that
+            suits your needs and preferences. 
+          </p>
+          <br></br>
+          <div className="flex flex-row gap-6">
+            <Benefit title="Convenient" description="Effortlessly search and reserve co-working spaces with our user-friendly platform."/>
+            <Benefit title="Flexible" description="Customize your workspace by adding facilities according to your specific needs."/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
