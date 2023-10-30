cc1d3e174c6c7e3c1bca196d799afdd4b9d6ea2e
TikhampornSky <tikhamporntan@gmail.com>
2023-10-30 14:34:03 +0700

    fix: placeAutoComplete style

--- src/components/placesAutoComplete.tsx
+++ src/components/placesAutoComplete.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import usePlacesAutocomplete, { SetValue, ClearSuggestions } from "use-places-autocomplete";
 
-export default function  PlacesAutocomplete ({ onAddressSelect, initialValue }: { onAddressSelect?: (address: string) => void, initialValue?: string}) {
+export default function PlacesAutocomplete({ onAddressSelect, initialValue }: { onAddressSelect?: (address: string) => void, initialValue?: string }) {
     const { ready, value, suggestions: { status, data }, setValue, clearSuggestions, } = usePlacesAutocomplete({
         requestOptions: { componentRestrictions: { country: 'th' } },
         debounce: 300,
@@ -12,10 +12,10 @@ export default function  PlacesAutocomplete ({ onAddressSelect, initialValue }: 
             setValue(initialValue, false);
         }
     }, [initialValue, setValue])
-  
+
     return (
-        <div>
-            <div id="input-box" className={`inline-flex w-full gap-x-2  ${ value == "" || value == initialValue ? 'rounded-default': 'rounded-t-2xl' } border bg-stone px-3 text-cyan`}>
+        <div className="relative w-full">
+            <div id="input-box" className={`inline-flex w-full gap-x-2  ${value == "" || value == initialValue ? 'rounded-default' : 'rounded-t-2xl'} border bg-stone px-3 text-cyan`}>
                 <label className="font-normal text-cyan"> Location </label>
                 <input
                     value={value}
@@ -29,17 +29,17 @@ export default function  PlacesAutocomplete ({ onAddressSelect, initialValue }: 
                     placeholder="Enter your address"
                 />
             </div>
-    
+
             {status === 'OK' && (
-                <div className="w-full">
-                    <div>{renderSuggestions({data, setValue, clearSuggestions, onAddressSelect})}</div>
-                </div>
+                    <div className="absolute z-50 w-full">
+                        {renderSuggestions({ data, setValue, clearSuggestions, onAddressSelect })}
+                    </div>
             )}
         </div>
     );
 };
 
-function renderSuggestions ({ data, setValue, clearSuggestions, onAddressSelect }: {data: google.maps.places.AutocompletePrediction[], setValue: SetValue, clearSuggestions: ClearSuggestions, onAddressSelect: ((address: string) => void) | undefined}) {
+function renderSuggestions({ data, setValue, clearSuggestions, onAddressSelect }: { data: google.maps.places.AutocompletePrediction[], setValue: SetValue, clearSuggestions: ClearSuggestions, onAddressSelect: ((address: string) => void) | undefined }) {
     return data.map((suggestion, index, array) => {
         const {
             place_id,
@@ -49,17 +49,17 @@ function renderSuggestions ({ data, setValue, clearSuggestions, onAddressSelect 
 
         return (
             <div
-            key={place_id}
-            onClick={() => {
-                document.getElementById("input-box")?.classList.remove("rounded-t-2xl");
-                document.getElementById("input-box")?.classList.add("rounded-default");
-                setValue(description, false);
-                clearSuggestions();
-                onAddressSelect && onAddressSelect(description);
-            }}
-            className={`${ index === array.length - 1 ? 'rounded-b-2xl': '' } border ${ index % 2 === 1 ? 'bg-ice' : '' } px-3 font-roboto text-base text-allports placeholder:text-jetstream hover:text-navy hover:drop-shadow-md  focus:border-transparent focus:outline-none`}
+                key={place_id}
+                onClick={() => {
+                    document.getElementById("input-box")?.classList.remove("rounded-t-2xl");
+                    document.getElementById("input-box")?.classList.add("rounded-default");
+                    setValue(description, false);
+                    clearSuggestions();
+                    onAddressSelect && onAddressSelect(description);
+                }}
+                className={`${index === array.length - 1 ? 'rounded-b-2xl' : ''} border ${index % 2 === 1 ? 'bg-ice' : 'bg-stone'} px-3 font-roboto text-base text-allports placeholder:text-jetstream hover:text-navy hover:drop-shadow-md  focus:border-transparent focus:outline-none`}
             >
-                <strong> {main_text} </strong> 
+                <strong> {main_text} </strong>
                 <small> {secondary_text} </small>
             </div>
         );
