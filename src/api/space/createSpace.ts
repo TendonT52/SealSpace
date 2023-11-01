74a45e6bdcd4e48108462244f9fa212e4e610f68
TendonT52 <tendon.t52@gmail.com>
2023-11-01 12:07:30 +0700

    CRUD space

--- src/api/space/createSpace.ts
+++ src/api/space/createSpace.ts
@@ -76,3 +76,109 @@ export async function createSpace(req: ReqCreateSpace): Promise<ResCreateSpace> 
     data: space,
   }
 }
+
+export async function getSpace(amenitie: string, location: string) {
+  const spaces = await prisma.space.findMany()
+  let amenities = spaces.map((space) => space.Amenities.split(/[\s,]+/).map((s) => s.toLowerCase()))
+  let locations = spaces.map((space) => space.location.split(/[\s,]+/).map((s) => s.toLowerCase()))
+  let result = []
+  for (let i = 0; i < spaces.length; i++) {
+    if (amenities[i].includes(amenitie.toLowerCase()) && locations[i].includes(location.toLocaleLowerCase())) {
+      result.push(spaces[i])
+    }
+  }
+  return result
+}
+
+export async function updateSpace(id: string, req: ReqCreateSpace) {
+  const cookieStore = cookies()
+  const token = cookieStore.get("access_token")
+  if (token == undefined) {
+    return {
+      ok: false,
+      message: "access token not found",
+    }
+  }
+
+  let claim
+  try {
+    claim = verifyAccessToken(token.value)
+  } catch (error) {
+    return {
+      ok: false,
+      message: "Invalid access token",
+    }
+  }
+  let space
+  try {
+    space = await prisma.space.update({
+      where: {
+        id: id,
+        hostId: claim.userId,
+      },
+      data: {
+        name: req.name,
+        location: req.location,
+        available: req.available,
+        Rooms: req.Rooms,
+        Amenities: req.Amenities,
+        Rules: req.Rules,
+        Community: req.Community,
+        latitude: req.latitude,
+        longitude: req.longitude,
+      },
+    })
+  } catch (error) {
+    return {
+      ok: false,
+      message: `Space with id ${id} not found`,
+    }
+  }
+
+  return {
+    ok: true,
+    message: "Space updated successfully",
+    data: space,
+  }
+}
+
+export async function deleteSpace(id: string) {
+  const cookieStore = cookies()
+  const token = cookieStore.get("access_token")
+  if (token == undefined) {
+    return {
+      ok: false,
+      message: "access token not found",
+    }
+  }
+
+  let claim
+  try {
+    claim = verifyAccessToken(token.value)
+  } catch (error) {
+    return {
+      ok: false,
+      message: "Invalid access token",
+    }
+  }
+  let space
+  try {
+    space = await prisma.space.delete({
+      where: {
+        id: id,
+        hostId: claim.userId,
+      },
+    })
+  } catch (error) {
+    return {
+      ok: false,
+      message: `Space with id ${id} not found`,
+    }
+  }
+
+  return {
+    ok: true,
+    message: "Space deleted successfully",
+    data: space,
+  }
+}
